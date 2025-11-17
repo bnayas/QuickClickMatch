5fc7a6fd2448758355b3fe76fce01e335d2d76b3
Bnaya Steinmetz <bnaya.steinmetz@gmail.com>
2025-11-17 14:28:28 +0200

    try to optimize rendering

--- flutter/lib/presentation/widgets/image_data.dart
+++ flutter/lib/presentation/widgets/image_data.dart
@@ -1,25 +1,25 @@
 // image_data.dart
-import 'package:image/image.dart' as img;
+import 'dart:typed_data';
 
 class ImageData {
   final String id;
   final String src;
   final List<PointData> contour;
-  img.Image? image;
+  Uint8List? bytes;
   ImageData(
-      {required this.id, required this.src, required this.contour, this.image});
+      {required this.id, required this.src, required this.contour, this.bytes});
 
   // Add a copyWith method for partial updates
   ImageData copyWith(
       {String? id,
       String? src,
       List<PointData>? contour,
-      img.Image? imageBytes}) {
+      Uint8List? bytes}) {
     return ImageData(
         id: id ?? this.id,
         src: src ?? this.src,
         contour: contour ?? this.contour,
-        image: image ?? this.image);
+        bytes: bytes ?? this.bytes);
   }
 
   factory ImageData.fromJson(Map<String, dynamic> json) {
